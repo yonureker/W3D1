353959a9659e0d6f8b796fb78967c5b911bbe01c
yonureker <yonureker@gmail.com>
2019-05-27 15:15:49 -0700

    Attempt 04a and solve 05

--- skeleton/lib/04a_countries_bonus.rb
+++ skeleton/lib/04a_countries_bonus.rb
@@ -17,6 +17,11 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE GDP > (SELECT MAX(gdp) FROM countries WHERE continent = 'Europe')
   SQL
 end
 
@@ -24,12 +29,39 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      max_by_area.continent, countries.name, max_by_area.max as area
+    FROM
+      countries
+    JOIN 
+      ( SELECT continent, MAX(area) as max
+      FROM countries
+      GROUP BY continent
+      ) max_by_area
+    ON max_by_area.max = countries.area
+
   SQL
+
+    # SELECT DISTINCT
+    # continent, name, area
+    # FROM countries
+    # ORDER BY area DESC
+    # LIMIT 8
 end
 
 def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT 
+      name, continent
+    FROM 
+      countries
+    
+
+    # SELECT name, continent, population from countries
+    # ORDER BY population DESC
+    # LIMIT 7
+
   SQL
 end
--- skeleton/lib/05_aggregates.rb
+++ skeleton/lib/05_aggregates.rb
@@ -22,30 +22,44 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+  SELECT DISTINCT continent
+  FROM countries
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+  SELECT SUM(gdp) AS total_gdp
+  FROM countries
+  WHERE continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+  SELECT COUNT(*)
+  FROM countries
+  WHERE area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+    SELECT SUM(population)
+    FROM countries
+    WHERE name IN ('France','Germany','Spain')
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT continent, COUNT(name)
+    FROM countries
+    GROUP BY continent
   SQL
 end
 
@@ -53,11 +67,21 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+    SELECT continent, COUNT(name)
+    FROM COUNTRIES 
+    WHERE population >= 10000000
+    group by continent
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
-  SQL
+    SELECT continent from (
+      SELECT continent, SUM(population) as sum
+      FROM COUNTRIES
+      GROUP BY continent
+    ) banana
+    WHERE banana.sum > 100000000
+    SQL
 end
